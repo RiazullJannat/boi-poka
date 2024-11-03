8cab755ea3a971b7c528a4e58fe2eb80fc7cdd4f
RJannat <ramimk097@gmail.com>
2024-11-03 17:22:31 +0600

    Added the sort by option

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -5,16 +5,34 @@ import { getReadList } from '../../utlities/readList/readList';
 import { useEffect, useState } from 'react';
 import Book from "../Book/Book";
 const ListedBooks = () => {
+    const [sort, setSort] = useState('');
     const [readBooks, setReadBooks] = useState([]);
     const allData = useLoaderData();
     useEffect(() => {
-        const readList =(getReadList().map(id=> parseInt(id)));
+        const readList = (getReadList().map(id => parseInt(id)));
         const readBooksData = allData.filter(book => readList.includes(book.bookId));
         setReadBooks(readBooksData);
     }, []);
+    const handleSort=(value)=>{
+        setSort(value)
+        if(value==='Ratings'){
+           setReadBooks( [...readBooks].sort((a,b)=> a.rating-b.rating) )
+        }
+        else if(value==='Page'){
+            setReadBooks( [...readBooks].sort((a,b)=> a.totalPages-b.totalPages) )
+        }
+    }
     return (
         <div>
-            <h1 className="2xl">Listed books.</h1>
+            <div className='flex justify-center'>
+                <details className="dropdown">
+                    <summary className="btn m-1">{sort? sort: 'Sort By'}</summary>
+                    <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                        <li onClick={()=>handleSort('Ratings')}><a>Ratings</a></li>
+                        <li onClick={()=>handleSort('Page')}><a>By Page</a></li>
+                    </ul>
+                </details>
+            </div>
             <Tabs>
                 <TabList>
                     <Tab>Read books</Tab>
